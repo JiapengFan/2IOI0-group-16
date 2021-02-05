a2ec09cf024101c72d59559925a5b0d88c65fb10
JiapengFan <j.fan@student.tue.nl>
2021-02-05 11:03:42 +0100

    Unix timestamp added

--- dataParsing.py
+++ dataParsing.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import datetime as dt
 
-df = pd.read_csv('data\BPI2012Test.csv')
+df_2012 = pd.read_csv('.\data\BPI2012Test.csv')
+df_Italy = pd.read_csv('.\data\BPI2012Test.csv')
 
-print(df)
+
+unix_time = df_2012['event time:timestamp'].apply(lambda x: dt.datetime.timestamp(dt.datetime.strptime(x, "%d-%m-%Y %H:%M:%S.%f")))
+
+# date = dt.datetime.strptime(date, "%d-%m-%Y %H:%M:%S.%f")
+
+# date = dt.datetime.timestamp(date)
+
+df_2012['unix_timestamp'] = unix_time
