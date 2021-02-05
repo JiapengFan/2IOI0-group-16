1f5ed6a6d014a3fedf1c155bdb115c2c72515516
JiapengFan <j.fan@student.tue.nl>
2021-02-05 12:56:02 +0100

    Data parsed
    Data parsed into df. Timestamps converted to unix time and grouped dataframe by case ID, number of events per case also added.

--- dataParsing.py
+++ dataParsing.py
@@ -1,14 +1,44 @@
 import pandas as pd
 import datetime as dt
 
+# Parse csv into dataframe
 df_2012 = pd.read_csv('.\data\BPI2012Test.csv')
 df_Italy = pd.read_csv('.\data\BPI2012Test.csv')
 
+# Parse time zone if there are any
+def convertTimezone(x):
+    if 'T' in x: 
+        without_timezone = x[:10] + ' ' + x[11:-6]
 
-unix_time = df_2012['event time:timestamp'].apply(lambda x: dt.datetime.timestamp(dt.datetime.strptime(x, "%d-%m-%Y %H:%M:%S.%f")))
+        if '.' in x:
+            without_timezone_unix = dt.datetime.timestamp(dt.datetime.strptime(without_timezone, "%Y-%m-%d %H:%M:%S.%f"))
+        else:
+            without_timezone_unix = dt.datetime.timestamp(dt.datetime.strptime(without_timezone, "%Y-%m-%d %H:%M:%S"))
 
-# date = dt.datetime.strptime(date, "%d-%m-%Y %H:%M:%S.%f")
+        if (x[-6]=='+'):
+            wholesomeTime = without_timezone_unix + int(x[-5:-3]) * 3600 + int(x[-2:]) * 60
+        else:
+            wholesomeTime = without_timezone_unix - int(x[-5:-3]) * 3600 - int(x[-2:]) * 60
+        
+    else:
+        if '.' in x:
+            wholesomeTime = dt.datetime.timestamp(dt.datetime.strptime(x, "%d-%m-%Y %H:%M:%S.%f"))
+        else:
+            wholesomeTime = dt.datetime.timestamp(dt.datetime.strptime(x, "%d-%m-%Y %H:%M:%S"))
 
-# date = dt.datetime.timestamp(date)
+    return wholesomeTime
 
-df_2012['unix_timestamp'] = unix_time
+# Convert event and reg timestamp into unix time
+df_2012['unix_event_time'] = df_2012['event time:timestamp'].apply(lambda x: convertTimezone(x))
+df_2012['unix_reg_time'] = df_2012['case REG_DATE'].apply(lambda x: convertTimezone(x))
+
+# Group data set by case ID 
+df_2012_grouped_by_case = df_2012.groupby(by=['case concept:name'])
+
+# Return data frame consisting out of the last event per case with column that indicates the number of events the case underwent appended
+df_2012_last_event_per_case = df_2012_grouped_by_case.nth([-1])
+df_2012_last_event_per_case['num_events'] = df_2012_grouped_object.count()['unix_event_time']
+
+# Inspect data frames
+print(df_2012_last_event_per_case, '\n')
+print(df_2012)
