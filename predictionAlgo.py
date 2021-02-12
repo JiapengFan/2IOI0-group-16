b193d40017b81a70f8ca8fbd99b5665b7ff32858
Quint Bakens <quint.bakens@hotmail.com>
2021-02-12 15:29:49 +0100

    Fixed time to next event predictor
    Added better functionality for this method.

--- predictionAlgo.py
+++ predictionAlgo.py
@@ -3,11 +3,11 @@ from dataParsing import parseData
 
 # Convert csv into dataframe
 df_2012 = pd.read_csv('.\data\BPI2012Training.csv')
-df_2012_Test = pd.read_csv('.\data\BPI2012Test.csv')
+df_2012_test = pd.read_csv('.\data\BPI2012Test.csv')
 
 # Parse data
 (df_2012, df_2012_last_event_per_case) = parseData(df_2012)
-(df_2012_Test, df_2012_last_event_per_case_Test) = parseData(df_2012_Test)
+(df_2012_test, df_2012_last_event_per_case_Test) = parseData(df_2012_test)
 
 
 # Function that naively predicts the avereage time that each case takes in days
@@ -24,16 +24,57 @@ def naiveAverageTimeOfCasePredictor(dataSet_last_event_per_case):
 
 
 # Function that naively predicts the time till the next event
-def naiveTimeToNextEventPredictor(dataSet_last_event_per_case):
+def naiveTimeToNextEventPredictor(dataSet, applyDataSet):
 
     # Create a safe working copy of the input dataset
-    df_prediction_temp = dataSet_last_event_per_case.copy()
+    df_prediction_temp = dataSet.copy()
 
-    # Add a new column which is the time between the last event and the first event divided by the number of events that took place
-    df_prediction_temp['timeToNextEvent'] = df_prediction_temp['unix_rel_event_time'] / \
-        df_prediction_temp['num_events']
+    # Dictionary holding the best prediction of the next event given the current event
+    bestPredicitonForEvent = {}
 
-    return df_prediction_temp
+    # Dictionary holding the amount of occurrences oe ach event after a given event
+    event_dictionary = {}
+
+    # Array holding the actual next events of each event in a case, if it is not the last event
+    actualNextEvent = []
+
+    # Series with all the unique events that are present in the dataset
+    unique_events = df_prediction_temp['event concept:name'].unique()
+
+    # Add all the keys to the event_dictionary and the bestPredictionForEvent dictionary
+    for key in unique_events:
+        event_dictionary[key] = [0, 0]
+        bestPredicitonForEvent[key] = 0
+
+    # Sort the temporary datafrime to use time analysis for each dase
+    df_predicted_next_event = df_prediction_temp.sort_values(
+        by=['case concept:name']).reset_index(drop=True)
+
+    # Get the amount of occurrences of each event after a specific event and store this information in the event_dictionary
+    for index, row in df_predicted_next_event.iterrows():
+        caseID = row['case concept:name']
+        try:
+            nextRow = df_predicted_next_event.iloc[index+1]
+        except:
+            continue
+        if (caseID != nextRow['case concept:name']):
+            continue
+        else:
+            #print(event_dictionary[row['event concept:name']])
+            event_dictionary[row['event concept:name']][0] += (
+                nextRow['unix_abs_event_time'] - row['unix_abs_event_time'])
+            event_dictionary[row['event concept:name']][1] += 1
+
+    for key in event_dictionary:
+        bestPredicitonForEvent[key] = event_dictionary[key][0] / \
+            event_dictionary[key][1]
+
+    print(bestPredicitonForEvent)
+
+    applyDataSet['timeToNextEvent'] = applyDataSet['event concept:name'].map(
+        bestPredicitonForEvent)
+
+    return applyDataSet
 
 
 # Function to naively predict the next event
@@ -112,6 +153,6 @@ def naiveNextEventPredictor(dataSet, applyDataSet):
     return(applyDataSet)
 
 
-print(naiveTimeToNextEventPredictor(df_2012_last_event_per_case).head(20))
-print(naiveNextEventPredictor(df_2012, df_2012_Test).head(20))
-print(naiveAverageTimeOfCasePredictor(df_2012_last_event_per_case))
+print(naiveTimeToNextEventPredictor(df_2012, df_2012_test).head(20))
+#print(naiveNextEventPredictor(df_2012, df_2012_test).head(20))
+# print(naiveAverageTimeOfCasePredictor(df_2012_last_event_per_case))
