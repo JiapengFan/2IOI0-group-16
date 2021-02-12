56dc7501b7a8761c0aac9b43f308a8d355b8ab33
JiapengFan <j.fan@student.tue.nl>
2021-02-12 20:44:26 +0100

    Update simple_testing.py

--- functions/simple_testing.py
+++ functions/simple_testing.py
@@ -3,11 +3,11 @@ import pandas as pd
 def MSEcalc(data, actualtime_col :str, prediction_col :str):
     total_mse = 0
     
-    for i in range(len(data)):
-        mse = ((data[prediction_col][i] - data[actualtime_col][i])^2)
+    for i in range(len(data) + 1):
+        mse = ((data[prediction_col][str(i)] - data[actualtime_col][str(i)])^2)
         total_mse += mse
     
-    return total_mse
+    return total_mse / len(data)
     
 def confusion_matrix_time(data, actualtime_col :str, prediction_col :str, error_margin):
     correct = 0
@@ -46,8 +46,8 @@ def event_accuracy(data, actualtime_event :str, prediction_event :str):
     for i in range(len(data)):
         if data[actualtime_event][i] == data[prediction_event][i]:
             acc += 1
-    
-    accuracy = acc/len(data) 
+
+    accuracy = acc/len(data)
     return accuracy
 
 def confusion_matrix_event(data, actual_col :str, prediction_col :str, events):
@@ -57,7 +57,7 @@ def confusion_matrix_event(data, actual_col :str, prediction_col :str, events):
         predicted_list = [0]*len(events)
         for row in range(len(data)):
             for idx, predicted_event in enumerate(events):
-                if (data[prediction_col][row] == predicted_event) and (data[actual_col][row] == actual_event) and (data[actual_col][row] == data[prediction_col][row]):
+                if (data[prediction_col][row] == predicted_event) and (data[actual_col][row] == actual_event):
                     predicted_list[idx]+=1
         df[actual_event] = predicted_list
     df = pd.DataFrame(df)
@@ -66,7 +66,7 @@ def confusion_matrix_event(data, actual_col :str, prediction_col :str, events):
 
     for x in range(0, len(events)):
         indices[x] = events[x]
-    
+
     df.rename(index = indices, inplace=True)
 
     return df
