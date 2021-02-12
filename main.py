7a504924b74084204693eeabafddb95ae0e94adb
JiapengFan <j.fan@student.tue.nl>
2021-02-12 18:16:19 +0100

    Added comments

--- main.py
+++ main.py
@@ -11,17 +11,23 @@ df_2012_Test = pd.read_csv('.\data\BPI2012Test.csv')
 (df_2012, df_2012_last_event_per_case) = parseData(df_2012)
 (df_2012_Test, df_2012_last_event_per_case_Test) = parseData(df_2012_Test)
 
+# Invoking predictors, see naming of functions for their purposes
 (dfPredictedEvent, uniqueEvents) = naiveNextEventPredictor(df_2012, df_2012_Test)
 dfPredictedTimePerCase = naiveAverageTimeOfCasePredictor(df_2012_last_event_per_case, df_2012_last_event_per_case_Test)
 dfPredictedTime = naiveTimeToNextEventPredictor(df_2012, df_2012_Test)
 
+# Validation process that returns its accuracy or a df in confusion matrix format
 mse_predicted_time_per_case = MSEcalc(dfPredictedTimePerCase, 'unix_rel_event_time', 'predictedTime')
 confusion_matrix_event(dfPredictedEvent, 'actualNextEvent', 'timeToNextEvent', uniqueEvents).to_pickle('confusion_matrix_event.pkl')  # where to save it, usually as a .pkl
-confusion_matrix_time(dfPredictedTime, 'actualNextEvent', 'predictedNextEvent', dfPredictedTime['unix_rel_event_time'].std()).to_pickle('confusion_matrix_time.pkl')
+# Commented out because of missing column with event's actual time
+# confusion_matrix_time(dfPredictedTime, 'actualNextEvent', 'predictedNextEvent', dfPredictedTime['unix_rel_event_time'].std()).to_pickle('confusion_matrix_time.pkl')
 
+# Read stored confusion matrices
 confusion_matrix_event = pd.read_pickle('confusion_matrix_event.pkl')
-confusion_matrix_time = pd.read_pickle('confusion_matrix_time.pkl')
+# See above for reason
+# confusion_matrix_time = pd.read_pickle('confusion_matrix_time.pkl')
 
+# Print out the accuracy or dfs in confusion matrix format
 print(mse_predicted_time_per_case, '\n')
 print(confusion_matrix_event, '\n')
 print(confusion_matrix_time, '\n')
