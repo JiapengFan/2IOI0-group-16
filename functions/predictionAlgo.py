b562b7f0f928ae1ba31ea9af7c16eb303bef986b
Quint Bakens <quint.bakens@hotmail.com>
2021-03-03 12:09:37 +0100

    Improved the predictions and improved efficiency
    Improved predictions and efficiency, also added time to next event column to df_training

--- functions/predictionAlgo.py
+++ functions/predictionAlgo.py
@@ -1,4 +1,8 @@
+import numpy as np
+
 # Function that naively predicts the avereage time that each case takes in days
+
+
 def naiveAverageTimeOfCasePredictor(dataSet_last_event_per_case_train, dataSet_last_event_per_case_test):
 
     # Create a safe working copy of the input dataset
@@ -16,124 +20,61 @@ def naiveAverageTimeOfCasePredictor(dataSet_last_event_per_case_train, dataSet_l
 # Function that naively predicts the time till the next event
 def naiveTimeToNextEventPredictor(dataSet, applyDataSet):
 
-    # Create a safe working copy of the input dataset
-    df_prediction_temp = dataSet.copy()
+    df_predicted_time_to_next_event = dataSet.copy().sort_values(
+        by=['case concept:name', "eventID ", "unix_abs_event_time"])
 
-    # Dictionary holding the best prediction of the next event given the current event
-    bestPredicitonForEvent = {}
+    df_predicted_time_to_next_event['actual_time_to_next_event'] = df_predicted_time_to_next_event['unix_abs_event_time'].shift(
+        -1) - df_predicted_time_to_next_event["unix_abs_event_time"]
 
-    # Dictionary holding the amount of occurrences oe ach event after a given event
-    event_dictionary = {}
+    df_predicted_time_to_next_event['actual_time_to_next_event'] = np.where((df_predicted_time_to_next_event['case concept:name'] == df_predicted_time_to_next_event['case concept:name'].shift(
+        -1)), df_predicted_time_to_next_event['actual_time_to_next_event'], np.nan)
+    # print(df_predicted_next_event.index.get_level_values(0))
 
-    # Series with all the unique events that are present in the dataset
-    unique_events = df_prediction_temp['event concept:name'].unique()
+    df_prediction_time_temp = df_predicted_time_to_next_event.copy()
+    df_prediction_time_temp_grouped = df_prediction_time_temp.groupby(
+        by="event concept:name").mean()["actual_time_to_next_event"]
 
-    # Add all the keys to the event_dictionary and the bestPredictionForEvent dictionary
-    for key in unique_events:
-        event_dictionary[key] = [0, 0]
-        bestPredicitonForEvent[key] = 0
+    df_prediction_time_temp['naive_predicted_time_to_next_event'] = df_prediction_time_temp['event concept:name'].map(
+        df_prediction_time_temp_grouped)
 
-    # Sort the temporary datafrime to use time analysis for each dase
-    df_predicted_next_event = df_prediction_temp.sort_values(
-        by=['case concept:name']).reset_index(drop=True)
+    applyDataSet['naive_predicted_time_to_next_event'] = applyDataSet['event concept:name'].map(
+        df_prediction_time_temp_grouped)
 
-    # Get the amount of occurrences of each event after a specific event and store this information in the event_dictionary
-    for index, row in df_predicted_next_event.iterrows():
-        caseID = row['case concept:name']
-        try:
-            nextRow = df_predicted_next_event.iloc[index+1]
-        except:
-            continue
-        if (caseID != nextRow['case concept:name']):
-            continue
-        else:
-            #print(event_dictionary[row['event concept:name']])
-            event_dictionary[row['event concept:name']][0] += (
-                nextRow['unix_abs_event_time'] - row['unix_abs_event_time'])
-            event_dictionary[row['event concept:name']][1] += 1
+    dataSet = df_prediction_time_temp
 
-    for key in event_dictionary:
-        bestPredicitonForEvent[key] = event_dictionary[key][0] / \
-            event_dictionary[key][1]
-
-    print(bestPredicitonForEvent)
-
-    applyDataSet['timeToNextEvent'] = applyDataSet['event concept:name'].map(
-        bestPredicitonForEvent)
-
-    return applyDataSet
+    return dataSet, applyDataSet
 
 # Function to naively predict the next event
+
+
 def naiveNextEventPredictor(dataSet, applyDataSet):
 
-    # Create a safe working copy of the input dataset
-    df_prediction_temp = dataSet.copy()
+    df_predicted_next_event = dataSet.copy().sort_values(
+        by=['case concept:name', "eventID ", "unix_abs_event_time"])
 
-    # Dictionary holding the best prediction of the next event given the current event
-    bestPredicitonForEvent = {}
+    df_predicted_next_event['actual_next_event'] = df_predicted_next_event["event concept:name"].shift(
+        -1)
 
-    # Dictionary holding the amount of occurrences oe ach event after a given event
-    event_dictionary = {}
+    df_predicted_next_event['actual_next_event'] = np.where((df_predicted_next_event['case concept:name'] == df_predicted_next_event['case concept:name'].shift(
+        -1)), df_predicted_next_event['actual_next_event'], np.nan)
 
-    # Array holding the actual next events of each event in a case, if it is not the last event
-    actualNextEvent = []
+    df_event_temp = df_predicted_next_event.copy()
+    unique_events = df_event_temp["event concept:name"].unique()
+    for x in unique_events:
+        df_event_temp[x] = np.where(
+            (df_event_temp["actual_next_event"] == x), 1, 0)
 
-    # List with all the unique events that are present in the dataset
-    unique_events = df_prediction_temp['event concept:name'].unique()
+    df_event_temp_grouped = df_event_temp.groupby(
+        by="event concept:name").sum()
 
-    # Add all the keys to the event_dictionary and the bestPredictionForEvent dictionary
-    for key in unique_events:
-        event_dictionary[key] = {}
-        bestPredicitonForEvent[key] = ""
-        for key2 in unique_events:
-            event_dictionary[key][key2] = 0
+    df_event_temp_grouped = df_event_temp_grouped[unique_events]
 
-    # Sort the temporary datafrime to use time analysis for each dase
-    df_predicted_next_event = df_prediction_temp.sort_values(
-        by=['case concept:name']).reset_index(drop=True)
+    df_predicted_next_event['naive_predicted_next_event'] = df_predicted_next_event['event concept:name'].map(
+        df_event_temp_grouped.idxmax(axis=1))
 
-    # Get the amount of occurrences of each event after a specific event and store this information in the event_dictionary
-    for index, row in df_predicted_next_event.iterrows():
-        caseID = row['case concept:name']
-        try:
-            nextRow = df_predicted_next_event.iloc[index+1]
-        except:
-            continue
-        else:
-            event_dictionary[row['event concept:name']
-                             ][nextRow['event concept:name']] += 1
+    applyDataSet['naive_predicted_next_event'] = applyDataSet['event concept:name'].map(
+        df_event_temp_grouped.idxmax(axis=1))
 
-    # Sort the temporary datafrime holding the testing data to use time analysis for each dase
-    df_actual_next_event_apply_dataset = applyDataSet.sort_values(
-        by=['case concept:name']).reset_index(drop=True)
+    dataSet = df_predicted_next_event
 
-    # Get the actual next events for the testing data
-    for index, row in df_actual_next_event_apply_dataset.iterrows():
-        caseID = row['case concept:name']
-        try:
-            nextRow = df_actual_next_event_apply_dataset.iloc[index+1]
-        except:
-            actualNextEvent.append("nan")
-            continue
-        if (caseID != nextRow['case concept:name']):
-            actualNextEvent.append("nan")
-            continue
-        else:
-            actualNextEvent.append(nextRow['event concept:name'])
-
-    # Calculate which event is the most likely to occur after each specific event
-    for key in event_dictionary:
-        currentMax = 0
-        for key2 in event_dictionary[key]:
-            if event_dictionary[key][key2] > currentMax:
-                currentMax = event_dictionary[key][key2]
-                bestPredicitonForEvent[key] = key2
-
-    # Add the predicted next evet to the applyDataset dataframe
-    applyDataSet['predictedNextEvent'] = applyDataSet['event concept:name'].map(
-        bestPredicitonForEvent)
-
-    # Add the actual next event to the dataframe
-    applyDataSet['actualNextEvent'] = actualNextEvent
-
-    return(applyDataSet, unique_events)
+    return dataSet, applyDataSet
